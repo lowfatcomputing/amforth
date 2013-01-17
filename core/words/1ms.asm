4540c7407dfa7649ee4d1fff9ef523c528615b46
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2013-01-17 20:06:33 +0000

    use the new delay macro
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1343 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/1ms.asm
+++ core/words/1ms.asm
@@ -8,17 +8,6 @@ VE_1MS:
     .set VE_HEAD = VE_1MS
 XT_1MS:
     .dw PFA_1MS
-    ; the delay loop core has 5 CPU cycles (or microseconds
-    ; at 1MHz. To reach 1ms, we need 200 cycles per MHz.
-    ; some constant overhead of the forth kernel as well
 PFA_1MS:
-    .set delay_cycles = F_CPU / 1000
-    ldi zl, LOW( delay_cycles / 5 )
-    ldi zh, HIGH(delay_cycles / 5 )
-    sbiw zl, 42 ; internal plus forth kernel overhead
-PFA_1MS1:
-    sbiw zl, 1
-    brts PFA_1MS2  ; leave if T-Flag is set, we need to deal with an interrupt
-    brne PFA_1MS1
-PFA_1MS2:
+    delay 1000
     jmp_ DO_NEXT
