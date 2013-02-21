17ed9a0b9108bf4507b6b90c4db7e669e170ad71
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2013-02-21 18:57:13 +0000

    additional checks for flags
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1371 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/spirw.asm
+++ core/words/spirw.asm
@@ -17,6 +17,9 @@ do_spirw:
     out_ SPDR, tosl
 do_spirw1:
     in_ temp0, SPSR
+    cbr temp0,7
+    out_ SPSR, temp0
+    in_ temp0, SPSR
     sbrs temp0, 7
     rjmp do_spirw1   ; wait until complete
     in_ tosl, SPDR
