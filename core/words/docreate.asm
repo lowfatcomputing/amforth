50bc32c05bbc0f5192ec2e98b4877ad4408d95ce
mtrute <mtrute@fd1c0b69-5d68-47f5-910c-86182ebb6ecd>
2013-03-31 16:46:02 +0000

    The scope word can change the word created.
    Added a small example.
    
    git-svn-id: svn://svn.code.sf.net/p/amforth/code/trunk@1405 fd1c0b69-5d68-47f5-910c-86182ebb6ecd

--- core/words/docreate.asm
+++ core/words/docreate.asm
@@ -10,8 +10,6 @@ XT_DOCREATE:
     .dw DO_COLON
 PFA_DOCREATE:
     .dw XT_PARSENAME
-    .dw XT_OVER
-    .dw XT_OVER
     .dw XT_WLSCOPE
     .dw XT_DUP
     .dw XT_TO_R
@@ -31,10 +29,13 @@ COLON_SMUDGE: .byte 4
 
 ; ( addr len -- wid )
 ; Compiler
-; wlscope, "wordlist scope", is a deferred word which enables AmForth
-; appl to choose the wordlist for a new voc entry based on its name.
-; For example, put all created words whose name begins with a tilde (~)
-; on a private wordlist. The default action is get-current.
+; wlscope, "wordlist scope" ( addr len -- addr' len' wid ), is a deferred word
+; which enables the AmForth application to choose the wordlist ( wid ) for the
+; new voc entry based on the input ( addr len ) string. The name of the new voc
+; entry ( addr' len' ) may be different from the input string. Note that all
+; created voc entry types pass through the wlscope mechanism. The default
+; wlscope action passes the input string to the output without modification and
+; uses get-current to select the wid.
 VE_WLSCOPE:
     .dw $ff07
     .db "wlscope",0
@@ -46,20 +47,3 @@ PFA_WLSCOPE:
     .dw EE_WLSCOPE
     .dw XT_EDEFERFETCH
     .dw XT_EDEFERSTORE
-
-; ( addr len -- wid )
-; Compiler
-; default scope
-VE_CURRENT_SCOPE:
-    .dw $ff0d
-    .db "current-scope",0
-    .dw VE_HEAD
-    .set VE_HEAD = VE_CURRENT_SCOPE
-XT_CURRENT_SCOPE:
-    .dw DO_COLON
-PFA_CURRENT_SCOPE:
-    .dw XT_DROP
-    .dw XT_DROP
-    .dw XT_GET_CURRENT
-    .dw XT_EXIT
-
